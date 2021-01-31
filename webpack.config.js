d831786fa382b23b571e8902c5ed1d2f4a8e2834
Seun Makinde <seunmakinde@ymail.com>
2021-01-31 05:56:02 -0500

    redirect with webpack copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
 	entry: './app/index.js',
@@ -19,6 +20,9 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: 'app/index.html'
+		}),
+		new CopyPlugin({
+			patterns: [{ from: '_redirects' }]
 		})
 	],
 	devServer: {
